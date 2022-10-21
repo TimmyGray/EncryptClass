3101f0cad7bcf225044457d72b416997aa1e3c09
TimmyGray <zuuubada@inbox.ru>
2022-10-21 16:15:30 +0300

    Add project files.

---
+++ EncryptClass/OwnDecrypt.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace EncryptClass
+{
+    public class OwnDecrypt
+    {
+
+        private long _key = 34543563123;
+
+        public string DecryptString(string encryptpassword)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char word in encryptpassword)
+            {
+               char secretchar =  (char)(word ^ _key);
+               sb.Append(secretchar);
+            }
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ EncryptClass/SysDecrypt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace EncryptClass
+{
+    public static class SysDecrypt
+    {
+
+        static readonly byte[] addentropy = { 2,4,5,7,5,1 };
+
+        public static string Decrypt(string encryptpass)
+        {
+
+            try
+            {
+                
+                byte[] bytes = ProtectedData.Unprotect(Convert.FromBase64String(encryptpass), addentropy, DataProtectionScope.LocalMachine);
+               
+                UnicodeEncoding unicode = new UnicodeEncoding();
+                
+                return unicode.GetString(bytes);
+            }
+            catch (CryptographicException e)
+            {
+
+                Console.WriteLine("Ошибка в дешифровке пароля");
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+
+
+    }
+}
